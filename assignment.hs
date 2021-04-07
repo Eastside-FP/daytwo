636f80e7b61967743c5d559ea574f337c2d49d3c
subifire5 <subifire@outlook.com>
2021-04-07 10:20:00 -0700

    DayTwo assignment done

--- assignment.hs
+++ assignment.hs
@@ -23,7 +23,7 @@ import Prelude
 -- returns the reciprocal of that number. This should make (1a) and (1b) 
 -- correct in the main function at the end
 
-reciprocal x = . . .
+reciprocal x = (1/x)
 
 
 -------
@@ -33,7 +33,7 @@ reciprocal x = . . .
 -- write a function square that squared a number. Uncomment (2) below
 -- and check it works.
 
--- your code here
+square x = x*x
 
 -------
 -- 3 --
@@ -43,7 +43,7 @@ reciprocal x = . . .
 -- that returns the reciprocal of the square of its parameter
 -- (that it, 1 / x²). Make sure (3) works.
 
--- your code
+oneOverXSquared x = reciprocal (square x)
 
 -------
 -- 4 --
@@ -56,7 +56,7 @@ reciprocal x = . . .
 -- Write a function that generates an infinite list of 1/x² for x = 1, 2, 3,…
 -- Call the function `series`. (4) below should now be correct
 
--- your code
+series = (map oneOverXSquared) [1..]
 
 -------
 -- 5 --
@@ -74,11 +74,11 @@ reciprocal x = . . .
 --  Write a function `sumOfFirst` that takes a number and a (possibily infinite)
 --  list of numbers and returns the sum of the first n elements
 --
---       sumOfFirat 5 [1..]     => 15
+--       sumOfFirst 5 [1..]     => 15
 --
 --  Uncomment (5) to verify
 
--- your code
+sumOfFirst x  y =  sum (take x y)
 
 -------
 -- 6 --
@@ -99,10 +99,10 @@ main = do
   print ("1a. Should be 0.5: " ++ (show $ reciprocal 2))
   print ("1b. Should be 4.0: "   ++ (show $ reciprocal 0.25))
 
-  -- print ("2. Should be 25: " ++ (show $ square 5))
+  print ("2. Should be 25: " ++ (show $ square 5))
 
-  -- print ("3. Should be 0.25: " ++ (show $ oneOverXSquared 2))
+  print ("3. Should be 0.25: " ++ (show $ oneOverXSquared 2))
 
-  -- print ("4. Should be [1.0, 0.25, 0.11.., 0.0625]: " ++ (show $ take 4 series))
+  print ("4. Should be [1.0, 0.25, 0.11.., 0.0625]: " ++ (show $ take 4 series))
 
-  -- print ("5. Should be roughly 1.4236: " ++ ( show $ sumOfFirst 4 series))
+  print ("5. Should be roughly 1.4236: " ++ ( show $ sumOfFirst 4 series))
